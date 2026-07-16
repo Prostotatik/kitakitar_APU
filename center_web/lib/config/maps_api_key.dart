ce81f5e3e4a0b1f4834b36785f8f944440e8a7df
ShawnLYZ <shawnlee0206@gmail.com>
2026-07-17 07:14:04 +0800

    bin_upgrade

--- center_web/lib/config/maps_api_key.dart
+++ center_web/lib/config/maps_api_key.dart
@@ -1,3 +1,3 @@
 /// Google Maps API key (same as in web/index.html and mobile android/local.properties).
 /// Used for Places Autocomplete and Geocoding HTTP requests.
-const String kGoogleMapsApiKey = 'AIzaSyC2yYREcvEu4e2IoLndDXEp7s5QyGdprbc';
+const String kGoogleMapsApiKey = 'YOUR GOOGLE MAP API KEY';
